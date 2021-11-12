9d072a5e8cd971f2a0ac3be47f6b1b4d96f8cc26
ttpro1995 <ttpro1995@yahoo.com.vn>
2021-11-13 00:22:48 +0700

    before missing dependencies error

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -1,8 +1,8 @@
 import { pinJSONToIPFS } from './pinata.js';
 require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-const web3 = createAlchemyWeb3(alchemyKey);
+// const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+// const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require('../contract-abi.json');
 const contractAddress = '0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE';
@@ -109,59 +109,59 @@ function addWalletListener() {
   }
 }
 
-export const mintNFT = async (url, name, description) => {
-  //error handling
-  if (url.trim() == '' || name.trim() == '' || description.trim() == '') {
-    return {
-      success: false,
-      status: '❗Please make sure all fields are completed before minting.',
-    };
-  }
+// export const mintNFT = async (url, name, description) => {
+//   //error handling
+//   if (url.trim() == '' || name.trim() == '' || description.trim() == '') {
+//     return {
+//       success: false,
+//       status: '❗Please make sure all fields are completed before minting.',
+//     };
+//   }
 
-  //make metadata
-  const metadata = new Object();
-  metadata.name = name;
-  metadata.image = url;
-  metadata.description = description;
+//   //make metadata
+//   const metadata = new Object();
+//   metadata.name = name;
+//   metadata.image = url;
+//   metadata.description = description;
 
-  //pinata pin request
-  const pinataResponse = await pinJSONToIPFS(metadata);
-  if (!pinataResponse.success) {
-    return {
-      success: false,
-      status: '😢 Something went wrong while uploading your tokenURI.',
-    };
-  }
-  const tokenURI = pinataResponse.pinataUrl;
+//   //pinata pin request
+//   const pinataResponse = await pinJSONToIPFS(metadata);
+//   if (!pinataResponse.success) {
+//     return {
+//       success: false,
+//       status: '😢 Something went wrong while uploading your tokenURI.',
+//     };
+//   }
+//   const tokenURI = pinataResponse.pinataUrl;
 
-  //load smart contract
-  window.contract = await new web3.eth.Contract(contractABI, contractAddress); //loadContract();
+//   //load smart contract
+//   window.contract = await new web3.eth.Contract(contractABI, contractAddress); //loadContract();
 
-  //set up your Ethereum transaction
-  const transactionParameters = {
-    to: contractAddress, // Required except during contract publications.
-    from: window.ethereum.selectedAddress, // must match user's active address.
-    data: window.contract.methods
-      .mintNFT(window.ethereum.selectedAddress, tokenURI)
-      .encodeABI(), //make call to NFT smart contract
-  };
+//   //set up your Ethereum transaction
+//   const transactionParameters = {
+//     to: contractAddress, // Required except during contract publications.
+//     from: window.ethereum.selectedAddress, // must match user's active address.
+//     data: window.contract.methods
+//       .mintNFT(window.ethereum.selectedAddress, tokenURI)
+//       .encodeABI(), //make call to NFT smart contract
+//   };
 
-  //sign transaction via Metamask
-  try {
-    const txHash = await window.ethereum.request({
-      method: 'eth_sendTransaction',
-      params: [transactionParameters],
-    });
-    return {
-      success: true,
-      status:
-        '✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/' +
-        txHash,
-    };
-  } catch (error) {
-    return {
-      success: false,
-      status: '😥 Something went wrong: ' + error.message,
-    };
-  }
-};
+//   //sign transaction via Metamask
+//   try {
+//     const txHash = await window.ethereum.request({
+//       method: 'eth_sendTransaction',
+//       params: [transactionParameters],
+//     });
+//     return {
+//       success: true,
+//       status:
+//         '✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/' +
+//         txHash,
+//     };
+//   } catch (error) {
+//     return {
+//       success: false,
+//       status: '😥 Something went wrong: ' + error.message,
+//     };
+//   }
+// };
